c90415f87e946c1ac54d3f8ba35fe9961278a6c6
Brendow Alves <brendowalves@hotmail.com>
2023-09-29 18:19:33 -0300

    improved the code

--- include/utils.h
+++ include/utils.h
@@ -1,12 +1,12 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/semphr.h"
-#include "freertos/ringbuf.h"
 #include "esp_err.h"
 #include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/ringbuf.h"
+#include "freertos/semphr.h"
+#include "freertos/task.h"
 
 #define ARGUNSED(x)
 #define FREQUENCY_IN_MILLIS 60000
--- main/tools/velocity_tool.c
+++ main/tools/velocity_tool.c
@@ -1,7 +1,7 @@
-#include "../../include/velocity_tool.h"
-#include "../../include/utils.h"
-#include "soc/rtc_wdt.h"
+#include "../include/velocity_tool.h"
+#include "../include/utils.h"
 #include "driver/gpio.h"
+#include "soc/rtc_wdt.h"
 
 #define TAG "velocity_tool"
 
@@ -13,65 +13,66 @@ gpio_config_t velocity_tool_gpio_config;
 SemaphoreHandle_t velocity_tool_sem;
 static TickType_t countTicks = 0;
 
-static void IRAM_ATTR velocity_tool_interrupt_handler(void *args)
-{
-    static BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-    countTicks = xTaskGetTickCountFromISR();
-    xSemaphoreGiveFromISR(velocity_tool_sem, pdFALSE);
-    if(xHigherPriorityTaskWoken) {
-        portYIELD_FROM_ISR();
-    }
+static void IRAM_ATTR velocity_tool_interrupt_handler(void *args) {
+  static BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+  countTicks = xTaskGetTickCountFromISR();
+  xSemaphoreGiveFromISR(velocity_tool_sem, pdFALSE);
+  if (xHigherPriorityTaskWoken) {
+    portYIELD_FROM_ISR();
+  }
 }
 
-static inline void set_gpio_velocity_tool()
-{
-    gpio_config(&velocity_tool_gpio_config);
-    gpio_install_isr_service(0);
-    gpio_isr_handler_add(VELOCITY_TOOL_PIN, velocity_tool_interrupt_handler, (void *)VELOCITY_TOOL_PIN);
+static inline void set_gpio_velocity_tool() {
+  gpio_config(&velocity_tool_gpio_config);
+  gpio_install_isr_service(0);
+  gpio_isr_handler_add(VELOCITY_TOOL_PIN, velocity_tool_interrupt_handler,
+                       (void *)VELOCITY_TOOL_PIN);
 }
 
-void velocity_tool_init(void *p1)
-{
-    float rpm = 0.0f;
-    float velocity = 0.0f;
-    uint64_t period_instants[2] = {0U};
-    int period_diff = 0;
-    char payload[20];
+void velocity_tool_init(void *p1) {
+  float rpm = 0.0f;
+  float velocity = 0.0f;
+  uint64_t period_instants[2] = {0U};
+  int period_diff = 0;
+  char payload[20];
 
-    ESP_LOGI(TAG, "Starting the velocity tool thread...");
-    RingbufHandle_t *ring_buf = (RingbufHandle_t *) p1;
-    set_gpio_velocity_tool();
+  ESP_LOGI(TAG, "Starting the velocity tool thread...");
+  RingbufHandle_t *ring_buf = (RingbufHandle_t *)p1;
+  set_gpio_velocity_tool();
 
-    velocity_tool_sem = xSemaphoreCreateCounting(PULSES_PER_REVOLUTION, 0);
-    if (velocity_tool_sem == NULL) {
-        goto error;
+  velocity_tool_sem = xSemaphoreCreateCounting(PULSES_PER_REVOLUTION, 0);
+  if (velocity_tool_sem == NULL) {
+    goto error;
+  }
+
+  while (true) {
+    xSemaphoreTake(velocity_tool_sem, portMAX_DELAY);
+    period_instants[0] = pdTICKS_TO_MS(countTicks);
+    xSemaphoreTake(velocity_tool_sem, portMAX_DELAY);
+    xSemaphoreTake(velocity_tool_sem, portMAX_DELAY);
+    xSemaphoreTake(velocity_tool_sem, portMAX_DELAY);
+    period_instants[1] = pdTICKS_TO_MS(countTicks);
+    period_diff = period_instants[1] - period_instants[0];
+    if (period_diff != 0) {
+      rpm = (float)(((float)(FREQUENCY_IN_MILLIS)) / period_diff);
+      velocity =
+          rpm * 0.104719f; /* pi/30(conversão do RPM) * raio da roda * 3.6 */
+      sprintf(payload, "velocity: %.5f", velocity);
+      ESP_LOGI(TAG, "period %d ms rpm %f velocity %f", period_diff, rpm,
+               velocity);
+      UBaseType_t rc = xRingbufferSend(*ring_buf, &payload, sizeof(payload),
+                                       pdMS_TO_TICKS(1));
+      if (rc != pdTRUE) {
+        printf("Failed to send item\n");
+      }
+    } else {
+      ESP_LOGE(TAG, "ERROR");
     }
+  }
 
-    while (true) {
-        xSemaphoreTake(velocity_tool_sem, portMAX_DELAY);
-        period_instants[0] = pdTICKS_TO_MS(countTicks);
-        xSemaphoreTake(velocity_tool_sem, portMAX_DELAY);
-        xSemaphoreTake(velocity_tool_sem, portMAX_DELAY);
-        xSemaphoreTake(velocity_tool_sem, portMAX_DELAY);
-        period_instants[1] = pdTICKS_TO_MS(countTicks);
-        period_diff = period_instants[1] - period_instants[0];
-        if(period_diff != 0 ) {
-            rpm = (float)(((float)(FREQUENCY_IN_MILLIS))/period_diff);
-            velocity = rpm * 0.104719f; /* pi/30(conversão do RPM) * raio da roda * 3.6 */
-            sprintf(payload, "velocity: %.5f", velocity);
-            ESP_LOGI(TAG, "period %d ms rpm %f velocity %f", period_diff, rpm, velocity);
-            UBaseType_t rc =  xRingbufferSend(*ring_buf, &payload, sizeof(payload), pdMS_TO_TICKS(1));
-            if (rc != pdTRUE) {
-                printf("Failed to send item\n");
-            }
-        } else {
-            ESP_LOGE(TAG, "ERROR");
-        }
-    }
-
-    error:
-        ESP_LOGI(TAG, "Error creating semaphore");
-        return;
+error:
+  ESP_LOGI(TAG, "Error creating semaphore");
+  return;
 }
 
 gpio_config_t velocity_tool_gpio_config = {
